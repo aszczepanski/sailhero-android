9efaaf66de12de4559e839e217eb7c2d6a03cd0c
Adam Szczepański <aszczepanski92@gmail.com>
2014-09-30 20:51:37 +0200

    response creator throws proper exceptions

--- src/put/sailhero/android/ResponseCreator.java
+++ src/put/sailhero/android/ResponseCreator.java
@@ -1,8 +1,14 @@
 package put.sailhero.android;
 
+import put.sailhero.android.exception.InvalidClientException;
+import put.sailhero.android.exception.InvalidRequestException;
+import put.sailhero.android.exception.InvalidResourceOwnerException;
+import put.sailhero.android.exception.InvalidResponseException;
+import put.sailhero.android.exception.UnsupportedGrantTypeException;
+
 
 public class ResponseCreator {
-	static public <T extends ProcessedResponse> T createFrom(HttpResponse response, Class<T> cls) throws Exception {
+	static public <T extends ProcessedResponse> T createFrom(HttpResponse response, Class<T> cls) throws InstantiationException, IllegalAccessException, InvalidResponseException, InvalidClientException, InvalidResourceOwnerException, UnsupportedGrantTypeException, InvalidRequestException {
 		T processedResponse = cls.newInstance();
 		
 		processedResponse.createFrom(response);
