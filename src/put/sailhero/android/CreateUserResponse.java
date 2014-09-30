8e02ea6a3e7de98298b385c4753ad01e4e46399d
Adam Szczepański <aszczepanski92@gmail.com>
2014-09-30 17:47:01 +0200

    create user and user profile responses work with user and yach external classes

--- src/put/sailhero/android/CreateUserResponse.java
+++ src/put/sailhero/android/CreateUserResponse.java
@@ -51,43 +51,4 @@ public class CreateUserResponse extends ProcessedResponse {
 		this.user = user;
 	}
 
-	public static class User {
-		private Integer id;
-		private String createdAt;  // TODO: change to datetime type
-		private String email;
-		private Integer yachId;
-		
-		public Integer getId() {
-			return id;
-		}
-
-		public void setId(Integer id) {
-			this.id = id;
-		}
-
-		public String getCreatedAt() {
-			return createdAt;
-		}
-
-		public void setCreatedAt(String createdAt) {
-			this.createdAt = createdAt;
-		}
-
-		public String getEmail() {
-			return email;
-		}
-
-		public void setEmail(String email) {
-			this.email = email;
-		}
-
-		public Integer getYachId() {
-			return yachId;
-		}
-
-		public void setYachId(Integer yachId) {
-			this.yachId = yachId;
-		}
-	}
-	
 }
