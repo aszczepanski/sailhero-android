7b01b28595d6e9b41def4fd585bb565a65133fca
Adam Szczepański <aszczepanski92@gmail.com>
2014-10-11 09:46:11 +0200

    unauthorized exception added

---
+++ src/put/sailhero/android/exception/UnauthorizedException.java
@@ -0,0 +1,11 @@
+package put.sailhero.android.exception;
+
+public class UnauthorizedException extends Exception {
+	public UnauthorizedException() {
+		super();
+	}
+
+	public UnauthorizedException(String message) {
+		super(message);
+	}
+}
