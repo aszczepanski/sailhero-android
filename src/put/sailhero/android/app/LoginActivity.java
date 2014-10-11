b2b3545cde6842cee0569df7d1580eb2e26373cb
Adam Szczepański <aszczepanski92@gmail.com>
2014-10-11 10:41:23 +0200

    authenticate user task

--- src/put/sailhero/android/app/LoginActivity.java
+++ src/put/sailhero/android/app/LoginActivity.java
@@ -1,11 +1,25 @@
 package put.sailhero.android.app;
 
 import put.sailhero.android.R;
+import put.sailhero.android.exception.InvalidResourceOwnerException;
+import put.sailhero.android.utils.AuthenticateUserRequest;
+import put.sailhero.android.utils.AuthenticateUserResponse;
+import put.sailhero.android.utils.AuthenticateUserResponseCreator;
+import put.sailhero.android.utils.Connection;
+import put.sailhero.android.utils.Request;
 import put.sailhero.android.utils.SailHeroService;
 import put.sailhero.android.utils.SailHeroSettings;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
@@ -31,6 +45,8 @@ public class LoginActivity extends Activity {
 	private SailHeroService mService;
 	private SailHeroSettings mSettings;
 
+	private Button mAuthenticateUserButton;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -51,6 +67,19 @@ public class LoginActivity extends Activity {
 		if (mSettings.getAccessToken() != null) {
 			onConnected();
 		}
+
+		mAuthenticateUserButton = (Button) findViewById(R.id.ActivityLoginAuthenticateUserButton);
+		mAuthenticateUserButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				EditText emailText = (EditText) findViewById(R.id.ActivityLoginEmailEditText);
+				EditText passwordText = (EditText) findViewById(R.id.ActivityLoginPasswordEditText);
+				AuthenticateUserRequest authenticateUserRequest = new AuthenticateUserRequest(emailText
+						.getText().toString().trim(), passwordText.getText().toString());
+				AuthenticateUserTask task = new AuthenticateUserTask(authenticateUserRequest);
+				task.execute();
+			}
+		});
 	}
 
 	@Override
@@ -66,5 +95,55 @@ public class LoginActivity extends Activity {
 		Intent intent = new Intent(LoginActivity.this, MainActivity.class);
 		startActivity(intent);
 		finish();
+	}
+
+	class AuthenticateUserTask extends AsyncTask<Void, Void, Void> {
+
+		private SailHeroService mService = SailHeroService.getInstance();
+		private Exception mException;
+		ProgressDialog mProgressDialog;
+		AuthenticateUserRequest mAuthenticateUserRequest;
+
+		public AuthenticateUserTask(AuthenticateUserRequest authenticateUserRequest) {
+			mAuthenticateUserRequest = authenticateUserRequest;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			mProgressDialog = ProgressDialog.show(LoginActivity.this, "Authenticating user",
+					"Executing request...");
+		}
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			Connection connection = mService.getConnection();
+			Request request;
+
+			request = mAuthenticateUserRequest;
+			AuthenticateUserResponse authenticateUserResponse;
+			try {
+				authenticateUserResponse = connection.send(request, new AuthenticateUserResponseCreator());
+				mSettings.setAccessToken(authenticateUserResponse.getAccessToken());
+				mSettings.save();
+				Log.d(TAG, "access token: " + mSettings.getAccessToken());
+			} catch (Exception e) {
+				mException = e;
+				e.printStackTrace();
+			}
+
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			mProgressDialog.dismiss();
+			if (mException == null) {
+				onConnected();
+			} else if (mException instanceof InvalidResourceOwnerException) {
+				Toast.makeText(LoginActivity.this, mException.getMessage(), Toast.LENGTH_LONG).show();
+			} else {
+				Toast.makeText(LoginActivity.this, "An error has occured.", Toast.LENGTH_LONG).show();
+			}
+		}
 	}
 }
