b67fdc000c7d6ccee772f7c1113ec6ad8f79299c
Adam Szczepański <aszczepanski92@gmail.com>
2014-11-02 11:06:59 +0100

    gcm initial commit

--- src/put/sailhero/android/app/MainActivity.java
+++ src/put/sailhero/android/app/MainActivity.java
@@ -1,5 +1,7 @@
 package put.sailhero.android.app;
 
+import java.io.IOException;
+
 import put.sailhero.android.R;
 import put.sailhero.android.util.GetPortsRequest;
 import put.sailhero.android.util.GetPortsResponse;
@@ -16,16 +18,28 @@ import put.sailhero.android.util.UserProfileRequest;
 import put.sailhero.android.util.UserProfileResponse;
 import put.sailhero.android.util.UserProfileResponseCreator;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
 public class MainActivity extends Activity {
 
 	public final static String TAG = "sailhero";
+
+	public static final String PROPERTY_REG_ID = "registration_id";
+	String SENDER_ID = "804800551458";
+	GoogleCloudMessaging gcm;
+	String regId;
+
+	Context context;
 
 	private SailHeroService mService;
 	private SailHeroSettings mSettings;
@@ -37,6 +51,89 @@ public class MainActivity extends Activity {
 
 		mService = SailHeroService.getInstance();
 		mSettings = mService.getSettings();
+
+		context = getApplicationContext();
+
+		regId = getRegistrationId(context);
+		if (regId.isEmpty()) {
+			registerInBackground();
+		}
+	}
+
+	private void registerInBackground() {
+		new AsyncTask<Void, Void, String>() {
+			@Override
+			protected String doInBackground(Void... params) {
+				String msg = "";
+				try {
+					if (gcm == null) {
+						gcm = GoogleCloudMessaging.getInstance(context);
+					}
+					regId = gcm.register(SENDER_ID);
+					msg = "Device registered, registration ID=" + regId;
+
+					sendRegistrationIdToBackend();
+
+					storeRegistrationId(context, regId);
+				} catch (IOException ex) {
+					msg = "Error: " + ex.getMessage();
+					// If there is an error, don't just keep trying to register.
+					// Require the user to click a button again, or perform
+					// exponential back-off.
+				}
+				return msg;
+			}
+
+			@Override
+			protected void onPostExecute(String msg) {
+				Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+				super.onPostExecute(msg);
+			}
+
+		}.execute(null, null, null);
+		// ...
+	}
+
+	private void sendRegistrationIdToBackend() {
+		// Your implementation here.
+	}
+
+	private String getRegistrationId(Context context) {
+		final SharedPreferences prefs = getGCMPreferences(context);
+		String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+		if (registrationId.isEmpty()) {
+			Log.i(TAG, "Registration not found.");
+			return "";
+		}
+
+		// Check if app was updated; if so, it must clear the registration ID
+		// since the existing regID is not guaranteed to work with the new
+		// app version.
+
+		// int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION,
+		// Integer.MIN_VALUE);
+		// int currentVersion = getAppVersion(context);
+		// if (registeredVersion != currentVersion) {
+		// Log.i(TAG, "App version changed.");
+		// return "";
+		// }
+
+		return registrationId;
+	}
+
+	private SharedPreferences getGCMPreferences(Context context) {
+		return getSharedPreferences(MainActivity.class.getSimpleName(), Context.MODE_PRIVATE);
+	}
+
+	private void storeRegistrationId(Context context, String regId) {
+		final SharedPreferences prefs = getGCMPreferences(context);
+		// int appVersion = getAppVersion(context);
+		// Log.i(TAG, "Saving regId on app version " + appVersion);
+		Log.i(TAG, "Saving regId");
+		SharedPreferences.Editor editor = prefs.edit();
+		editor.putString(PROPERTY_REG_ID, regId);
+		// editor.putInt(PROPERTY_APP_VERSION, appVersion);
+		editor.commit();
 	}
 
 	@Override
