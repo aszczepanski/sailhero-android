a357b1444f1c0c115298a77323def7c9302701a3
Adam Szczepański <aszczepanski92@gmail.com>
2015-01-07 17:59:04 +0100

    finish item in dashboard's menu

--- src/put/sailhero/ui/DashboardActivity.java
+++ src/put/sailhero/ui/DashboardActivity.java
@@ -260,7 +260,8 @@ public class DashboardActivity extends BaseActivity implements GooglePlayService
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_finish) {
+			finish();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
